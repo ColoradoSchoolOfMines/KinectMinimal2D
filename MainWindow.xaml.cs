c71586bfa4b4a054c83345d4b32c6c2942b39dc9
alanchampion <alanchampion@gmail.com>
2014-09-20 15:35:18 -0600

    Initial Commit
    This is a basic skeleton project for a 2D Kinect game using C# and WPF.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,218 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// A bitmap buffer to put pixel data from the Kinect into for rendering by the WPF Image
+        /// </summary>
+        private WriteableBitmap colorBitmap;
+
+        /// <summary>
+        /// A byte array to put raw image data from the Kinect into for transformation into a bitmap
+        /// </summary>
+        private byte[] pixelBytes;
+
+        /// <summary>
+        /// A brush that can be used to draw things on screen in the specified color
+        /// </summary>
+        private readonly Brush basicColorBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
+
+        /// <summary>
+        /// A pen that can be used to draw things on screen using the specified brush and width
+        /// </summary>        
+        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor sensor;
+
+        /// <summary>
+        /// Drawing group for skeleton rendering output
+        /// </summary>
+        private DrawingGroup drawingGroup;
+
+        /// <summary>
+        /// Drawing image that we will display
+        /// </summary>
+        private DrawingImage imageSource;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Execute startup tasks - Use this for setup that needs to occur after a Kinect is available
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Create the drawing group we'll use for drawing
+            this.drawingGroup = new DrawingGroup();
+
+            // Create an image source that we can use in our image control
+            this.imageSource = new DrawingImage(this.drawingGroup);
+
+            // Display the drawing using our image control
+            Canvas.Source = this.imageSource;
+
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            // Ensure that we have a sensor before continuing
+            if (null != this.sensor)
+            {
+                // Turn on the proper streams to receive event frames
+                // Disable lines for event streams you are not using
+                this.sensor.SkeletonStream.Enable();
+                this.sensor.ColorStream.Enable();
+                this.sensor.DepthStream.Enable();
+
+                // Add an event handlers to be called whenever there is new color frame data
+                // Disable event handler registrations for events you are not using
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+                this.sensor.AllFramesReady += this.SensorAllFramesReady;
+                this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+                this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+
+                // Prepare the image byte buffer and the bitmap pixel buffer, then bind the bitmap to the WPF Image canvas
+                this.pixelBytes = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                Canvas.Source = colorBitmap;
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            // No sensor, complain
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = Properties.Resources.NoKinectReady;
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks - Use this to release resources as the game shuts down
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's SkeletonFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            // Acquire data from the SkeletonFrameReadyEventArgs...
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+
+            // ...and do stuff with it!
+            using (DrawingContext dc = this.drawingGroup.Open())
+            {
+                // Do any drawing here with the acquired DrawingContext and skeleton information
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's ColorFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            // Acquire data from ColorImageFrameReadyEventArgs and do stuff with it
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (null != colorFrame)
+                {
+                    using (DrawingContext dc = this.drawingGroup.Open())
+                    {
+                        // Copy the pixel data from the image to a temporary array
+                        colorFrame.CopyPixelDataTo(this.pixelBytes);
+
+                        // Write the pixel data into our bitmap
+                        this.colorBitmap.WritePixels(
+                            new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                            this.pixelBytes,
+                            this.colorBitmap.PixelWidth * sizeof(int),
+                            0);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the Kinect sensor's DepthFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            // Acquire data from DepthImageFrameReadyEventArgs and do stuff with it
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's AllFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            // Acquire data from AllFramesReadyEventArgs and do stuff with it
+        }
+    }
+}
